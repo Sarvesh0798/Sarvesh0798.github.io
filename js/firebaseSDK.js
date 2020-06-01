705cf183046b43d2d660e0652d3d61a3d66e6797
Sarvesh0798 <sarveshkothekar8@gmail.com>
2020-06-01 13:12:40 +0530

    5th commit

--- js/firebaseSDK.js
+++ js/firebaseSDK.js
@@ -2,12 +2,7 @@
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "sorry",
-    authDomain: "portfolio-1e14e.firebaseapp.com",
-    databaseURL: "https://portfolio-1e14e.firebaseio.com",
-    projectId: "portfolio-1e14e",
-    storageBucket: "portfolio-1e14e.appspot.com",
-    messagingSenderId: "766319374997",
-    appId: "1:766319374997:web:94e4a10bfb290839ec619c"
+   
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
